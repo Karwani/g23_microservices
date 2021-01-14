8b1250add8b91572a4e8300f75d84d97e42fc615
32monkeys <38853524+32monkeys@users.noreply.github.com>
2021-01-14 15:59:58 +0100

    more progress

--- client/src/test/java/clientside/AccountSteps.java
+++ client/src/test/java/clientside/AccountSteps.java
@@ -1,10 +1,46 @@
 package clientside;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import javax.ws.rs.client.WebTarget;
 
 public class AccountSteps {
+    WebTarget baseURL;
 
-    @Given("a new customer with id {string}")
-    
+    public AccountSteps(){
+        
+    }
+
+    @Given("a new customer with id ”{string}”")
+    public void aNewCustomerWithId(String userId) {
+
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("name {string1} {string2}")
+    public void name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user initiates registration")
+    public void userInitiatesRegistration() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the type is {string}")
+    public void theTypeIs() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("a new user is created")
+    public void aNewUserIsCreated() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
 }
