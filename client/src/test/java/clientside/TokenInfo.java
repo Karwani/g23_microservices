fb51be1e7019ddb8e0bb191960e4dd40383190e0
Laura Petre <laura.p1012@gmail.com>
2021-01-15 14:25:45 +0200

    ppayment server refactoring

--- client/src/test/java/clientside/TokenInfo.java
+++ client/src/test/java/clientside/TokenInfo.java
@@ -1,8 +1,17 @@
 package clientside;
 
+import java.util.Random;
+
 public  class TokenInfo {
 
     public static String tokenId;
     public static String userId;
+    public static String generateRandomCPR()
+    {
+        int p1 = 100000 + new Random().nextInt(900000);
+
+        int p2 = 1000 + new Random().nextInt(9000);
+        return p1 + "-" + p2;
+    }
 
 }
